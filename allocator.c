1eb0ba469f5163730d208fda1888f14b4bf18763
Roman Skurikhin <roman.skurikhin@cruxlab.com>
2019-06-26 15:17:49 +0300

    Make realloc more efficient

--- allocator.c
+++ allocator.c
@@ -1,33 +1,33 @@
 #include <stdint.h>
 #include <stddef.h>
 
-// SYSTEM 
-int brk(void *addr);
+// SYSTEM
+/**
+ * Here defined all platform-specific code.
+ * We only use sbrk to control memory available and
+ * memcpy for reallocation needs.
+ */
 void *sbrk(intptr_t increment);
-
-
 #include <memory.h>
+#include <errno.h>
 // !SYSTEM
-
-void *malloc(size_t size);
-void free(void *ptr);
 
 // Config constants
 #define BINS_NUM		128
-#define EXACT_BINS		64
 #define CHUNK_ALIGN		16		// Should be power of two
 #define PAGE_SIZE		4096	// Should be power of two
 #define USED_CHUNK_BIT  1
+#define RETURN_MEM_THRESHOLD 10 * 1024 * 1024
 
 // Align macros
-/*
+/**
  * Main ideas behind this bit magic is that power of two minus 1 looks like this:
  * 000...00011..11 in binary. So the ~ (bitwise not) will be like this:
  * 111...11100..00 in binary. When you will use & (bitwise and) with another
  * number, you will set to zero lower bits. This will make number have
- * zero reminder for some power of two (...10 is divisable by two, ...100 is 
- * divisable by four, ...1000 is divisable by 8, and so on). As we started from
- * some power of two, result will be divisable by those number.
+ * zero reminder for some power of two (...10 is divisible by two, ...100 is
+ * divisible by four, ...1000 is divisible by 8, and so on). As we started from
+ * some power of two, result will be divisible by those number.
  * 
  * For upper align, we just add align factor - 1 to the number and align lower
  */
@@ -37,20 +37,22 @@ void free(void *ptr);
 #define align_page_top(n)  align_page_lower(n + PAGE_SIZE - 1)
 
 // Structs definitions
-/*
+/**
  * During allocation/frees, memory will be divided in chunks, and each chunk
  * will have some size. In this implementation, all sizes will be aligned to CHUNK_ALIGN.
  * All chunks will have header and footer (part of memory before and after users
- * part of memory). Both of them will hold size of the chunk, so it will be easy to
+ * memory). Both of them will hold size of the chunk, so it will be easy to
  * traverse memory (when you know beginning of one chunk, you have an access of its size,
- * size and location of previous and size and location of next chunk). 
- * Chunk can be of two types - free and allocated for user ones.
+ * size and location of previous and size and location of next chunk).
+ * Chunk can be of two types - free and allocated for user ones. The size will have
+ * special bit mark USED_CHUNK_BIT set if this is allocated for user chunk, and it will be cleared
+ * if it is free chunk.
  * Two organize free chunks (to be able effectively find chunk to allocate), chunks of same size
  * will grouped in "bin" - collection of almost-same-sized chunks. Chunks in bin will be stored as
  * two-way linked list. Each bin have pointer to first chunk in bin (or NULL if bin is empty), 
  * and each chunks will have two links - to previous and to the next chunk in bin. First chunk in bin 
  * will have NULL previous link, last chunk will have NULL next link.
- * Chunks inside of the bin will be sorted by their size, from smaller to bigger ones. Also bin
+ * Chunks inside of the bin will be sorted by their size, from bigger to smaller ones. Also bin
  * size is upper bound (including) of chunks stored it in, so sizes of chunks follows next rule:
  * bins[bin - 1].size < chunk.size <= bins[bin].size
  *
@@ -70,7 +72,7 @@ typedef struct bin_header {
 } bin_header_t;
 
 // Static data definitions
-/*
+/**
  * Here we define structure to store bin headers, and bounds of memory.
  * It is important to track both of this values, as allocator can go off the
  * memory when checking neighbour chunks.
@@ -86,25 +88,23 @@ void *memory_begin, *memory_end;
 void *end_gap_begin = NULL;
 
 // Size macros
-/*
+/**
  * The smallest users request is one byte, but to allocate it we need to store 
  * header and footer, so we have to add size of used header and footer, and align it to CHUNK_ALIGN.
  * But we can not use it as MIN_SIZE, as when user will free this chunk, then we
- * will need to store free header at front. 
- * As free header is larger then used footer, we can simply sum size of 
- * free header and free footer.
+ * will need to store free header at front.
  */
 #define MIN_SIZE align_chunk_top(sizeof(free_header_t) + sizeof(footer_t))
 
 // Init code
-/*
+/**
  * The most important part here is algorithm to generate sizes of bins. The conception is
  * to have difference between neighbour bins in first half of all bins equal CHUNK_ALIGN,
  * then next quarter to have difference CHUNK_ALIGN ** 2, the 1/8 with difference 
  * CHUNK_ALIGN ** 3, and so on. For example, if CHUNK_ALIGN is 8 and we have 128 bins, then
  * first 64 bins will be MIN_SIZE, MIN_SIZE + 8, MIN_SIZE + 16, MIN_SIZE + 24, ...,
  * MIN_SIZE + 512, then there next will be MIN_SIZE + 512 + 64, MIN_SIZE + 512 + 2 * 64, and
- * so on (hope you have get an idea)
+ * so on.
  * The import note here is last bin - it's size will be always SIZE_MAX, so all huge chunks
  * will find theirs place there. 
  */
@@ -137,22 +137,35 @@ void init() {
 
 // Main code
 
-/*
+/**
  * To allocate memory, we need to find how much memory we need (to find correct
- * bin for it), and it have to include both headers, be aligned and be not less
- * then MIN_SIZE (sorry for describing code in words here)
+ * bin for it), and it should include both headers, be aligned to CHUNK_ALIGN and
+ * be at least MIN_SIZE.
+ * @param size Size of memory requested by user
+ * @return Size of chunk to allocate
  */
 size_t request_size_to_allocated(size_t size) {
 	size_t with_overhead = align_chunk_top(size + sizeof(used_header_t) + sizeof(footer_t));
 	return with_overhead > MIN_SIZE ? with_overhead : MIN_SIZE;
 }
 
+/**
+ * Find bin number that should contain chunk of given size.
+ * TODO: replace loop with something more efficient
+ * @param size Size of chunk
+ * @return bin that should contain this chunk
+ */
 int to_bin_number(size_t size) {
 	int bin = 0;
 	while (bins[bin].size < size) bin++;
 	return bin;
 }
 
+/**
+ * Removes chunk from bin. This method properly updates links in linked list.
+ * @param bin Bin that contains given chunk
+ * @param chunk pointer to the chunk to delete
+ */
 void remove_chunk_from_bin(int bin, free_header_t *chunk) {
     if (chunk->next != NULL) {
         chunk->next->prev = chunk->prev;
@@ -165,17 +178,32 @@ void remove_chunk_from_bin(int bin, free_header_t *chunk) {
     }
 }
 
+/**
+ * Setup up given memory region as a free chunk in some bin.
+ * It properly fills data to header and footer, finds appropriate bin
+ * and inserts chunk to the bin preserving ordering (chunks inside one bin
+ * order descending by their sizes)
+ * ! size should be already aligned to CHUNK_ALIGN
+ * @param start Pointer to the begging of future chunk's memory region
+ * @param size Size of future chunk. Should be aligned to CHUNK_ALIGN
+ */
 void add_new_chunk_to_bin(void* start, size_t size) {
+    // At first, we prepare header/footer pointers
     free_header_t *new_chunk = start;
     footer_t *footer = start + size - sizeof(footer_t);
-    new_chunk->size = size;
-    *footer = size;
+    // Then we write chunks size to header/footer
+    new_chunk->size = *footer = size;
+    // Now we have to find bin were we should store new chunk
     int bin = to_bin_number(size);
     if (bins[bin].first == NULL) {
+        // If bin is empty, we simply add new chunk
         bins[bin].first = new_chunk;
         new_chunk->prev = NULL;
         new_chunk->next = NULL;
     } else {
+        // We have to find place in linked list to store new chunk
+        // To do this, we traverse list unit next chunk is smaller the new one,
+        // or list is ended.
         free_header_t *prev_chunk = NULL;
         free_header_t *next_chunk = bins[bin].first;
         while (next_chunk != NULL && size < new_chunk->size) {
@@ -183,6 +211,8 @@ void add_new_chunk_to_bin(void* start, size_t size) {
             next_chunk = next_chunk->next;
         }
         if (prev_chunk == NULL) {
+            // New chunk is larger then bin's first chunk - we have to update bin's
+            // pointer to the head
             bins[bin].first = new_chunk;
         } else {
             prev_chunk->next = new_chunk;
@@ -190,31 +220,35 @@ void add_new_chunk_to_bin(void* start, size_t size) {
         if (next_chunk != NULL) {
             next_chunk->prev = new_chunk;
         }
+        // In next to lines NULL values is ok - first chunk should have NULL prev,
+        // and last chunk should have NULL next.
         new_chunk->prev = prev_chunk;
         new_chunk->next = next_chunk;
     }
 }
 
-#define return_threshold 10 * 1024 * 1024
-
+/**
+ * Returns memory back to platform if size of end_gap is larger than RETURN_MEM_THRESHOLD.
+ */
 void return_memory_if_needed() {
     size_t end_gap_size = (size_t)(memory_end - end_gap_begin);
-    if (end_gap_size > return_threshold) {
-        size_t space_to_free = end_gap_size - return_threshold;
+    if (end_gap_size > RETURN_MEM_THRESHOLD) {
+        size_t space_to_free = end_gap_size - RETURN_MEM_THRESHOLD;
         sbrk(-space_to_free);
         memory_end -= space_to_free;
     }
 }
 
 void *malloc(size_t size) {
-    check_init();
-	size = request_size_to_allocated(size);
-	int bin = to_bin_number(size);
+    check_init(); // We have to check that bin's system inited before doing anything.
+	size = request_size_to_allocated(size); // Get size of chunk we should allocate
+	int bin = to_bin_number(size); // Get bin that can contain chunk we need
 	free_header_t *chunk = NULL;
 	/*
-	 * Here we trying to find chunk in bin. We go through bins and store smallest one.
+	 * Here we trying to find chunk in bin. We go through bins and store smallest chunk from those, which is
+	 * large enough.
 	 * This done by next using using next rules:
-	 * - chunks in bin are sorted from bigger to smaller, so we have to take last that is big enough.
+	 * - chunks in one bin are sorted from bigger to smaller, so we have to take last that is big enough.
 	 * - all chunks in one bin are smaller then chunks in next bin, so if we found chunk, we should not
 	 * check other bins.
 	 */
@@ -225,56 +259,62 @@ void *malloc(size_t size) {
 		    bin_chunk = bin_chunk->next;
 		}
 		if (chunk == NULL) {
-		    // Go to next bin if we did not find chunk
+		    // Go to next bin if we didn't find chunk
             bin++;
         }
 	}
     if (chunk != NULL) {
         // If we found chunk, we have to remove it from bin.
         remove_chunk_from_bin(bin, chunk);
-        // If chunk is to large, we have to split it
+        // If chunk is too large, we have to split it,
         if (chunk->size - size > MIN_SIZE) {
+            // as chunk->size and size are aligned to CHUNK_ALIGN, then difference is aligned to.
             add_new_chunk_to_bin((void *)chunk + size, chunk->size - size);
-            chunk->size = size;
+        } else {
+            // If difference is too small, then we process it like initial request was a little bin larger.
+            size = chunk->size;
         }
-        // Also we update length of allocated memory region
-        size = chunk->size;
 	}
 	if (chunk == NULL) {
         /*
-         * If chunk wasn't found, we have to work with memory end border. First we check,
-         * do we have enough space in end gap. If it is not enough, we get more from OS Memory
-         * manager. Then we prepare chunk and move end gap.
+         * If chunk wasn't found, we have to work with memory end border. First we check
+         * do we have enough space in end gap. If it is not enough, we get more from sbrk.
+         * Then we prepare chunk and move end gap.
          */
         size_t end_gap_size = memory_end - end_gap_begin;
 		if (end_gap_size < size) {
+		    // As memory is allocated by pages, it is better to align it.
 			size_t sbrk_size = align_page_top(size - end_gap_size);
-			void * end = sbrk(sbrk_size);
+			sbrk(sbrk_size);
             memory_end += sbrk_size;
 		}
 		chunk = end_gap_begin;
 		end_gap_begin += size;
 	}
+	// Prepare used header and footer and fill size with USED_CHUNK_BIT mark to them.
     used_header_t *allocated = (used_header_t *) chunk;
-    footer_t *allocated_footer = (footer_t *) ((void *)chunk + size - sizeof(used_header_t));
+    footer_t *allocated_footer = (footer_t *) ((void *)chunk + size - sizeof(footer_t));
 	*allocated = size | USED_CHUNK_BIT;
 	*allocated_footer = size | USED_CHUNK_BIT;
+	// Return pointer to the start of the user's section of chunk
 	return ((void *)allocated) + sizeof(used_header_t);
 }
 
 void free(void *ptr) {
-    check_init();
+    check_init(); // We have to check that bin's system inited before doing anything.
     if (ptr == NULL) {
         return;
     }
     // First, we get size of memory area we are freeing
     used_header_t *header = ptr - sizeof(used_header_t);
     size_t size = *header & (~USED_CHUNK_BIT);
-    // While previous memory area contains free chunk, we remove thoses chunk from bin and
+    // While previous/next memory area contains free chunk, we remove those chunk from bin and
     // grab all memory to return it all as free chunk.
+    // First, we prepare pointers to the previous chunk footer and next chunk header.
     footer_t *prev_chunk_end = ((void *)header) - sizeof(footer_t);
     free_header_t *next_chunk = ((void *)header) + size;
     while (memory_begin < (void *)prev_chunk_end && (*prev_chunk_end & USED_CHUNK_BIT) == 0) {
+        // If previous chunk is free chunk, we remove it from bin and go to the next previous chunk
         size_t prev_chunk_size = *prev_chunk_end;
         free_header_t *prev_chunk = (void *)prev_chunk_end - prev_chunk_size + sizeof(footer_t);
         remove_chunk_from_bin(to_bin_number(prev_chunk_size), prev_chunk);
@@ -282,37 +322,62 @@ void free(void *ptr) {
         prev_chunk_end = (void *)prev_chunk - sizeof(footer_t);
     }
     while ((void *)next_chunk < end_gap_begin && (next_chunk->size & USED_CHUNK_BIT) == 0) {
+        // Here we do same for next chunks.
         size_t next_chunk_size = next_chunk->size;
         remove_chunk_from_bin(to_bin_number(next_chunk_size), next_chunk);
         size += next_chunk_size;
         next_chunk = (void *)next_chunk + next_chunk_size;
     }
-    // Check, did we get to the end_gap_begin
     if (next_chunk == end_gap_begin) {
+        // If current free memory regions touches end_gap, then we give this memory
+        // to the end gap, not to the bin system.
         end_gap_begin -= size;
         return_memory_if_needed();
     } else {
+        // Return chunk to the bins system.
         add_new_chunk_to_bin((void *) prev_chunk_end + sizeof(footer_t), size);
     }
 }
 
 void *calloc(size_t nmemb, size_t size) {
+    if (nmemb > SIZE_MAX / size) {
+        // set errno here if platform has it
+        return NULL;
+    }
     return malloc(nmemb * size);
 }
 
 void *realloc(void *ptr, size_t size) {
-    size_t old_size = *(used_header_t *)(ptr - sizeof(used_header_t));
-    void *new_addr = malloc(size);
-    size_t copy_size = size;
-    if (old_size < copy_size) {
-        copy_size = old_size;
+    used_header_t *old_header = ptr - sizeof(used_header_t);
+    size_t old_size = *old_header & (~USED_CHUNK_BIT);
+    size_t new_chunk_size = request_size_to_allocated(size);
+    if (old_size < new_chunk_size) {
+        // New size is bigger, we have to allocate new region
+        void *new_addr = malloc(size);
+        size_t copy_size = size;
+        if (old_size < copy_size) {
+            copy_size = old_size;
+        }
+        memcpy(new_addr, ptr, copy_size);
+        free(ptr);
+        return new_addr;
+    } else {
+        size_t free_size = old_size - new_chunk_size;
+        if (free_size > MIN_SIZE) {
+            // We can use difference as free chunk
+            footer_t *new_footer = (void *)old_header + new_chunk_size - sizeof(footer_t);
+            *old_header = *new_footer = new_chunk_size | USED_CHUNK_BIT;
+            add_new_chunk_to_bin((void *)new_footer + sizeof(footer_t), free_size);
+        }
+        return ptr;
     }
-    memcpy(new_addr, ptr, copy_size);
-    free(ptr);
-    return new_addr;
 }
 
 void *reallocarray(void *ptr, size_t nmemb, size_t size) {
+    if (nmemb > SIZE_MAX / size) {
+        // set errno here if platform has it
+        return NULL;
+    }
     return realloc(ptr, nmemb * size);
 }
 
--- tests/test_multipe_allocs.c
+++ tests/test_multipe_allocs.c
@@ -8,10 +8,14 @@ int z = 0;
 int *arrs[maxn];
 size_t sizes[maxn];
 
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+
 int main() {
     srand(0);
-	for (int q = 0; q < 100000; q++) {
-	    printf("Entries cnt %d\n", z);
+	for (int q = 0; q < 1000; q++) {
+	    printf("Step %d\n", q);
 	    int allocs_cnt = rand() % 20;
         for (int i = 0; i < allocs_cnt; i++) {
             int alloc_size = rand() % (500000) + 10;
@@ -40,6 +44,27 @@ int main() {
             for (int j = del_index; j < z; j++) {
                 arrs[j] = arrs[j + 1];
                 sizes[j] = sizes[j + 1];
+            }
+        }
+
+        int realloc_cnt = rand() % 20;
+        if (z < realloc_cnt) {
+            realloc_cnt = z;
+        }
+        for (int i = 0; i < realloc_cnt; i++) {
+            int realloc_index = rand() % z;
+            int realloc_size = rand() % (500000) + 10;
+            arrs[realloc_index] = realloc(arrs[realloc_index], realloc_size * sizeof(int));
+            int min_size = min(realloc_size, sizes[realloc_index]);
+            sizes[realloc_index] = realloc_size;
+            for (int j = 0; j < min_size; j++) {
+                if (arrs[realloc_index][j] !=  0xffffffff) {
+                    printf("Data wasn't same on check:\n on position %d is %d, expected %d", j, arrs[realloc_index][j], 0xffffffff);
+                    return 1;
+                }
+            }
+            for (int j = 0; j < realloc_size; j++) {
+                arrs[realloc_index][j] = 0xffffffff;
             }
         }
     }
